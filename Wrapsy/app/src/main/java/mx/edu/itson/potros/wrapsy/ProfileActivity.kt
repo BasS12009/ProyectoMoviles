4dc45baac20d991cb8bb66864258b511d71ebe1f
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-03-27 03:49:56 -0700

    Listo 👀

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileActivity.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileActivity.kt
@@ -2,11 +2,10 @@ package mx.edu.itson.potros.wrapsy
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Profile : BaseActivity() {
+class ProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
