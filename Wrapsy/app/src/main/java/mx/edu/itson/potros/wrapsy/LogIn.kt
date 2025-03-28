60603b5c94097b7a0d51397a6864b64c6621fff8
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-03-27 17:49:51 -0700

    Navegacion login

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/LogIn.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/LogIn.kt
@@ -1,19 +1,33 @@
 package mx.edu.itson.potros.wrapsy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LogIn : AppCompatActivity() {
+class LogIn : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLogin: Button = findViewById(R.id.btn_login)
+        val btnSignUp: TextView = findViewById(R.id.btn_sign_up)
+
+        btnLogin.setOnClickListener (){
+            val intent = Intent(this, StoresActivity::class.java)
+            startActivity(intent)
         }
+
+        btnSignUp.setOnClickListener(){
+            val intent = Intent(this, SingUp::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
