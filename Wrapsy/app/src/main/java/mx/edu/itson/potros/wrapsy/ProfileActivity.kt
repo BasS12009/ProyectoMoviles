79b7990eb11dfc3bd086db79c85da7343c4787f1
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-27 17:15:26 -0700

    Navegación de pantallas

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileActivity.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileActivity.kt
@@ -1,6 +1,8 @@
 package mx.edu.itson.potros.wrapsy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -8,14 +10,30 @@ import androidx.core.view.WindowInsetsCompat
 class ProfileActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
         setupBottomNavigation()
         setSelectedItem(R.id.nav_profile)
+
+        val btnMoreOptions: ImageView = findViewById(R.id.more_options)
+        val btnNotification: ImageView = findViewById(R.id.btn_notificaciones)
+        val btnEditar: ImageView = findViewById(R.id.iv_edit_name)
+
+        btnMoreOptions.setOnClickListener(){
+            val intent = Intent(this, MoreOptions::class.java)
+            startActivity(intent)
+
+        }
+
+        btnNotification.setOnClickListener(){
+            val intent = Intent(this, NotificationsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEditar.setOnClickListener(){
+            val intent = Intent(this, ProfileEditProfile::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileEditProfile.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProfileEditProfile.kt
@@ -1,20 +1,36 @@
 package mx.edu.itson.potros.wrapsy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ProfileEditProfile : AppCompatActivity() {
+class ProfileEditProfile : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile_edit_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnNotification: ImageView = findViewById(R.id.btn_notificaciones)
+        val btnMoreOption:ImageView = findViewById(R.id.more_options)
+
+
+        btnNotification.setOnClickListener(){
+            val intent = Intent(this, NotificationsActivity::class.java)
+            startActivity(intent)
+
+        }
+        btnMoreOption.setOnClickListener(){
+            val intent = Intent(this, MoreOptions::class.java)
+            startActivity(intent)
+
         }
     }
 }
