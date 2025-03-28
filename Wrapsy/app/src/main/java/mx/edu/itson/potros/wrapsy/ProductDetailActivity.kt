ffb63424d34fa990fd36ff4fd1a84686ff52c76d
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-03-27 17:21:24 -0700

    detailProduct

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProductDetailActivity.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/ProductDetailActivity.kt
@@ -1,20 +1,30 @@
 package mx.edu.itson.potros.wrapsy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ProductDetailActivity : AppCompatActivity() {
+class ProductDetailActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_product_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btn_Basket:Button = findViewById(R.id.btn_add_to_basket)
+        setupBottomNavigation()
+
+        btn_Basket.setOnClickListener(){
+            val intent = Intent(this, BasketActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
