913bd1a71137c5eee1bef0b581ff2cf18dfe7778
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-05-06 23:57:32 -0700

    Autenticacion lista
    Ya se creó la conexion mediante correo. Falta trabajar con la auth de Gmail/Google.

---
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/Address.kt
@@ -0,0 +1,9 @@
+package mx.edu.itson.potros.wrapsy
+
+data class Address(
+    val country: String? = null,
+    val state: String? = null,
+    val city: String? = null,
+    val zip: String? = null,
+    val address: String? = null
+)
---
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/User.kt
@@ -0,0 +1,9 @@
+package mx.edu.itson.potros.wrapsy
+
+data class User(
+    val id: String? = null,
+    val name: String? = null,
+    val username: String? = null,
+    val email: String? = null,
+    val phone: String? = null
+)
