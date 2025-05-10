610b3082d62189b099f60158cca6418cc162f442
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-05-10 01:05:44 -0700

    Update Basket.kt

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/Entities/Basket.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/Entities/Basket.kt
@@ -8,18 +8,5 @@ data class Basket(
     val totalPrice: Double = 0.0
 ) {
 
-    fun recalculateTotalPrice(): Basket {
-        val sum = gifts.sumOf { it.price }
-        return this.copy(totalPrice = sum)
-    }
 
-    fun addGift(gift: Gift): Basket {
-        gifts.add(gift)
-        return recalculateTotalPrice()
-    }
-
-    fun removeGift(gift: Gift): Basket {
-        gifts.remove(gift)
-        return recalculateTotalPrice()
-    }
 }
