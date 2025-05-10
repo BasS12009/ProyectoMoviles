b296814d41afa8beb81172697bcdcced969d1a9b
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2025-05-09 21:16:02 -0700

    Logs

--- Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/BasketActivity.kt
+++ Wrapsy/app/src/main/java/mx/edu/itson/potros/wrapsy/BasketActivity.kt
@@ -3,6 +3,7 @@ package mx.edu.itson.potros.wrapsy
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.GridView
 import com.google.gson.Gson
@@ -21,6 +22,7 @@ class BasketActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("BasketActivity", "onCreate() started") // <-- AGREGAR ESTE LOG
         setContentView(R.layout.activity_basket)
 
         setupBottomNavigation()
@@ -29,7 +31,11 @@ class BasketActivity : BaseActivity() {
         basketListView = findViewById(R.id.basket_list_view)
         sharedPreferences = getSharedPreferences("basket_prefs", Context.MODE_PRIVATE)
 
+        Log.d("BasketActivity", "Views initialized") // <-- AGREGAR ESTE LOG
+
         loadBasketItems()
+
+        Log.d("BasketActivity", "loadBasketItems() called") // <-- VERIFICAR SI SE LLEGA AQUÍ
 
         giftBasketAdapter = GiftBasketAdapter(this, basketItems)
         basketListView.adapter = giftBasketAdapter
@@ -40,16 +46,25 @@ class BasketActivity : BaseActivity() {
     }
 
     private fun loadBasketItems() {
+        Log.d("BasketActivity", "loadBasketItems() started") // <-- AGREGAR ESTE LOG
         val basketListJson = sharedPreferences.getString("basket_items", null)
+        Log.d("BasketActivity", "Raw JSON from SharedPreferences: $basketListJson")
         basketItems.clear()
         if (!basketListJson.isNullOrEmpty()) {
             val type = object : TypeToken<ArrayList<Gift>>() {}.type
             val storedBasketItems: ArrayList<Gift>? = gson.fromJson(basketListJson, type)
             if (storedBasketItems != null) {
                 basketItems.addAll(storedBasketItems)
+                Log.d("BasketActivity", "Number of items loaded: ${basketItems.size}")
+                basketItems.forEach { Log.d("BasketActivity", "Loaded item: ${it.name}") }
+            } else {
+                Log.d("BasketActivity", "Stored basket items were null after Gson.");
             }
+        } else {
+            Log.d("BasketActivity", "No basket items found in SharedPreferences.");
         }
     }
+
 
     fun removeItemFromBasket(giftToRemove: Gift) {
         basketItems.remove(giftToRemove)
